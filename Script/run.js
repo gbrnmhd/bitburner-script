e47492b1ea442c434f798ff7fd18d75f876a82b4
Gilbran <gilmavira@gmail.com>
2024-03-20 22:50:18 +0700

    Update run.js

--- Script/run.js
+++ Script/run.js
@@ -1,17 +1,31 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  if(ns.hasRootAccess == true) ns.run(ns.args[0], 1, 'n00dles');
-  if(ns.hasRootAccess == true) ns.run(ns.args[0], 1, 'foodnstuff');
-  if(ns.hasRootAccess == true) ns.run(ns.args[0], 1, 'sigma-cosmetics');
-  if(ns.hasRootAccess == true) ns.run(ns.args[0], 1, 'joesguns');
-  if(ns.hasRootAccess == true) ns.run(ns.args[0], 1, 'hong-fang-tea');
-  if(ns.hasRootAccess == true) ns.run(ns.args[0], 1, 'harakiri-sushi');
-  if(ns.hasRootAccess == true) ns.run(ns.args[0], 1, 'iron-gym');
-  if(ns.hasRootAccess == true) ns.run(ns.args[0], 1, 'max-hardware');
-  if(ns.hasRootAccess == true) ns.run(ns.args[0], 1, 'nectar-net');
-  if(ns.hasRootAccess == true) ns.run(ns.args[0], 1, 'zer0');
-  if(ns.hasRootAccess == true) ns.run(ns.args[0], 1, 'silver-helix');
-  if(ns.hasRootAccess == true) ns.run(ns.args[0], 1, 'phantasy');
-  if(ns.hasRootAccess == true) ns.run(ns.args[0], 1, 'neo-net');
-  if(ns.hasRootAccess == true) ns.run(ns.args[0], 1, 'omega-net');
+  if(ns.hasRootAccess("n00dles") == true)
+    ns.run(ns.args[0], 1, 'n00dles');
+  if(ns.hasRootAccess("foodnstuff") == true)
+    ns.run(ns.args[0], 1, 'foodnstuff');
+  if(ns.hasRootAccess("sigma-cosmetics") == true) 
+    ns.run(ns.args[0], 1, 'sigma-cosmetics');
+  if(ns.hasRootAccess("joesguns") == true) 
+    ns.run(ns.args[0], 1, 'joesguns');
+  if(ns.hasRootAccess("hong-fang-tea") == true) 
+    ns.run(ns.args[0], 1, 'hong-fang-tea');
+  if(ns.hasRootAccess("harakiri-sushi") == true) 
+    ns.run(ns.args[0], 1, 'harakiri-sushi');
+  if(ns.hasRootAccess("iron-gym") == true) 
+    ns.run(ns.args[0], 1, 'iron-gym');
+  if(ns.hasRootAccess("max-hardware") == true) 
+    ns.run(ns.args[0], 1, 'max-hardware');
+  if(ns.hasRootAccess("nectar-net") == true) 
+    ns.run(ns.args[0], 1, 'nectar-net');
+  if(ns.hasRootAccess("zer0") == true) 
+    ns.run(ns.args[0], 1, 'zer0');
+  if(ns.hasRootAccess("silver-helix") == true) 
+    ns.run(ns.args[0], 1, 'silver-helix');
+  if(ns.hasRootAccess("phantasy") == true) 
+    ns.run(ns.args[0], 1, 'phantasy');
+  if(ns.hasRootAccess("neo-net") == true) 
+    ns.run(ns.args[0], 1, 'neo-net');
+  if(ns.hasRootAccess("omega-net") == true) 
+    ns.run(ns.args[0], 1, 'omega-net');
 }
