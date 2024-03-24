f703ed9f622f55f517cb5bd2f1544bb58b1729af
Gilbran <gilmavira@gmail.com>
2024-03-24 22:49:05 +0700

    update

--- Script/only-hack.js
+++
@@ -1,7 +0,0 @@
-/** @param {NS} ns */
-export async function main(ns) {
-  //run only-hack.js [target]
-  while(true){
-    await ns.hack(ns.args[0]);
-  }
-}
--- Script/run.js
+++ Script/run.js
@@ -1,32 +1,184 @@
 /** @param {NS} ns */
 export async function main(ns) {
   // run run.js [script]
-  if(ns.hasRootAccess("n00dles") == true)
+  if (ns.serverExists("n00dles") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("n00dles"))
     ns.run(ns.args[0], 1, 'n00dles');
-  if(ns.hasRootAccess("foodnstuff") == true)
+  }
+  if (ns.serverExists("foodnstuff") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("foodnstuff"))
     ns.run(ns.args[0], 1, 'foodnstuff');
-  if(ns.hasRootAccess("sigma-cosmetics") == true) 
+  }
+  if (ns.serverExists("sigma-cosmetics") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("sigma-cosmetics"))
     ns.run(ns.args[0], 1, 'sigma-cosmetics');
-  if(ns.hasRootAccess("joesguns") == true) 
+  }
+  if (ns.serverExists("joesguns") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("joesguns"))
     ns.run(ns.args[0], 1, 'joesguns');
-  if(ns.hasRootAccess("hong-fang-tea") == true) 
+  }
+  if (ns.serverExists("hong-fang-tea") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("hong-fang-tea"))
     ns.run(ns.args[0], 1, 'hong-fang-tea');
-  if(ns.hasRootAccess("harakiri-sushi") == true) 
+  }
+  if (ns.serverExists("harakiri-sushi") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("harakiri-sushi"))
     ns.run(ns.args[0], 1, 'harakiri-sushi');
-  if(ns.hasRootAccess("iron-gym") == true) 
+  }
+  if (ns.serverExists("iron-gym") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("iron-gym"))
     ns.run(ns.args[0], 1, 'iron-gym');
-  if(ns.hasRootAccess("max-hardware") == true) 
+  }
+  if (ns.serverExists("max-hardware") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("max-hardware"))
     ns.run(ns.args[0], 1, 'max-hardware');
-  if(ns.hasRootAccess("nectar-net") == true) 
+  }
+  if (ns.serverExists("nectar-net") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("nectar-net"))
     ns.run(ns.args[0], 1, 'nectar-net');
-  if(ns.hasRootAccess("zer0") == true) 
+  }
+  if (ns.serverExists("zer0") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("zer0"))
     ns.run(ns.args[0], 1, 'zer0');
-  if(ns.hasRootAccess("silver-helix") == true) 
+  }
+  if (ns.serverExists("silver-helix") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("silver-helix"))
     ns.run(ns.args[0], 1, 'silver-helix');
-  if(ns.hasRootAccess("phantasy") == true) 
+  }
+  if (ns.serverExists("phantasy") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("phantasy"))
     ns.run(ns.args[0], 1, 'phantasy');
-  if(ns.hasRootAccess("neo-net") == true) 
+  }
+  if (ns.serverExists("neo-net") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("neo-net"))
     ns.run(ns.args[0], 1, 'neo-net');
-  if(ns.hasRootAccess("omega-net") == true) 
+  }
+  if (ns.serverExists("omega-net") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("omega-net"))
     ns.run(ns.args[0], 1, 'omega-net');
+  }
+  if (ns.serverExists("johnson-ortho") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("johnson-ortho"))
+    ns.run(ns.args[0], 1, 'johnson-ortho');
+  }
+  if (ns.serverExists("crush-fitness") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("crush-fitness"))
+    ns.run(ns.args[0], 1, 'crush-fitness');
+  }
+  if (ns.serverExists("the-hub") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("the-hub"))
+    ns.run(ns.args[0], 1, 'the-hub');
+  }
+  if (ns.serverExists("computek") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("computek"))
+    ns.run(ns.args[0], 1, 'computek');
+  }
+  if (ns.serverExists("rothman-uni") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("rothman-uni"))
+    ns.run(ns.args[0], 1, 'rothman-uni');
+  }
+  if (ns.serverExists("netlink") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("netlink"))
+    ns.run(ns.args[0], 1, 'netlink');
+  }
+  if (ns.serverExists("catalyst") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("catalyst"))
+    ns.run(ns.args[0], 1, 'catalyst');
+  }
+  if (ns.serverExists("lexo-corp") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("lexo-corp"))
+    ns.run(ns.args[0], 1, 'lexo-corp');
+  }
+  if (ns.serverExists("snap-fitness") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("snap-fitness"))
+    ns.run(ns.args[0], 1, 'snap-fitness');
+  }
+  if (ns.serverExists("rho-construction") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("rho-construction"))
+    ns.run(ns.args[0], 1, 'rho-construction');
+  }
+  if (ns.serverExists("aevum-police") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("aevum-police"))
+    ns.run(ns.args[0], 1, 'aevum-police');
+  }
+  if (ns.serverExists("galactic-cyber") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("galactic-cyber"))
+    ns.run(ns.args[0], 1, 'galactic-cyber');
+  }
+  if (ns.serverExists("deltaone") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("deltaone"))
+    ns.run(ns.args[0], 1, 'deltaone');
+  }
+  if (ns.serverExists("zb-def") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("zb-def"))
+    ns.run(ns.args[0], 1, 'zb-def');
+  }
+  if (ns.serverExists("infocomm") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("infocomm"))
+    ns.run(ns.args[0], 1, 'infocomm');
+  }
+  if (ns.serverExists("defcomm") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("defcomm"))
+    ns.run(ns.args[0], 1, 'defcomm');
+  }
+  if (ns.serverExists("omnia") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("omnia"))
+    ns.run(ns.args[0], 1, 'omnia');
+  }
+  if (ns.serverExists("aerocorp") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("aerocorp"))
+    ns.run(ns.args[0], 1, 'aerocorp');
+  }
+  if (ns.serverExists("millenium-fitness") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("millenium-fitness"))
+    ns.run(ns.args[0], 1, 'millenium-fitness');
+  }
+  if (ns.serverExists("solaris") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("solaris"))
+    ns.run(ns.args[0], 1, 'solaris');
+  }
+  if (ns.serverExists("unitalife") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("unitalife"))
+    ns.run(ns.args[0], 1, 'unitalife');
+  }
+  if (ns.serverExists("global-pharm") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("global-pharm"))
+    ns.run(ns.args[0], 1, 'global-pharm');
+  }
+  if (ns.serverExists("alpha-ent") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("alpha-ent"))
+    ns.run(ns.args[0], 1, 'alpha-ent');
+  }
+  if (ns.serverExists("syscore") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("syscore"))
+    ns.run(ns.args[0], 1, 'syscore');
+  }
+  if (ns.serverExists("taiyang-digital") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("taiyang-digital"))
+    ns.run(ns.args[0], 1, 'taiyang-digital');
+  }
+  if (ns.serverExists("zeus-med") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("zeus-med"))
+    ns.run(ns.args[0], 1, 'zeus-med');
+  }
+  if (ns.serverExists("univ-energy") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("univ-energy"))
+    ns.run(ns.args[0], 1, 'univ-energy');
+  }
+  if (ns.serverExists("nova-med") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("nova-med"))
+    ns.run(ns.args[0], 1, 'nova-med');
+  }
+  if (ns.serverExists("icarus") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("icarus"))
+    ns.run(ns.args[0], 1, 'icarus');
+  }
+  if (ns.serverExists("summit-uni") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("summit-uni"))
+    ns.run(ns.args[0], 1, 'summit-uni');
+  }
+  if (ns.serverExists("zb-institute") == true) {
+    if(ns.getHackingLevel() >= ns.getServerRequiredHackingLevel("zb-institute"))
+    ns.run(ns.args[0], 1, 'zb-institute');
+  }
 }
